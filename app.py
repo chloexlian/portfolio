5aa7954bd720c541a1dde8ee9b967f2657377320
chloexlian <chloelian8@gmail.com>
2026-04-12 17:01:27 -0700

    Add projects page

--- app.py
+++ app.py
@@ -79,6 +79,10 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 @app.route("/blog")
 def blog():
     category = request.args.get("category")
